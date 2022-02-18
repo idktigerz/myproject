14ee4f2501b1e4b0b8f2e7ca02b9361011c06561
idktigerz <idktigerzz@gmail.com>
2022-02-18 09:17:35 +0000

    commit

--- models/connection.js
+++ models/connection.js
@@ -2,6 +2,16 @@ var pg = require('pg');
 
 const connectionString = process.env.DATABASE_URL
 const Pool = pg.Pool
+
+const pool = new Pool({
+    connectionString,
+    max: 10,
+    ssl: {
+        require: true, 
+        rejectUnauthorized: false
+    }
+})
+/*
 const pool = (() => {
     if (process.env.NODE_ENV !== 'production') {
         return new Pool({
@@ -16,4 +26,5 @@ const pool = (() => {
               }
         });
 } })();
+*/
 module.exports = pool
